266a855466da4c6a5418455ea19648b18b56110c
rishabh <rishabh7890@gmail.com>
2019-05-28 17:42:02 -0400

    Updates

---
+++ precache-manifest.78dd6f4f92b4e3befa87c2b13b23ef1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4ab4c226c3e59f5246c2e333439e29",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "d68baec02e63dfee5088",
+    "url": "/ClickyGame/static/css/main.a4e93e63.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/ClickyGame/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "d68baec02e63dfee5088",
+    "url": "/ClickyGame/static/js/main.63da25fc.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
